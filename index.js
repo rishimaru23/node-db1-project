90452b28cdd08dd31fee1a62113a387cbcaa6760
Ryan Ishimaru <rishimaru23@gmail.com>
2021-07-12 19:11:23 -0700

    wip

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server.js");
 
-const PORT = process.env.PORT || 1001;
+const PORT = process.env.PORT || 1002;
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
