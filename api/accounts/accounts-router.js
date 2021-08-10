c2e95051f41d7cd580dcb1a7ac34a53df7a3e11a
Ryan Ishimaru <rishimaru23@gmail.com>
2021-08-10 13:11:24 -0700

    mvp

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -39,10 +39,9 @@ router.put(
   md.checkAccountId,
   md.checkAccountPayload,
   async (req, res, next) => {
+    try {
     const updated = await Account.updateById(req.params.id, req.body)
     res.json(updated)
-    try {
-    res.json('update account')
   } catch (err) {
     next(err)
   }
